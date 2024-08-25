a06ed5502a12913f02b5cf70c3b7fe91727aec7b
varalakshmiy <yvaralakshmi04@gmail.com>
2024-08-25 18:31:10 +0530

    addition 2 num

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 echo "pls enter your username"
-read USERNAME # this is variable where the value of one user
+read -s USERNAME # this is variable where the value of one user
 echo "username entered" $USERNAME
+
+echo "pls enter your pwd"
+read -s PASSWORD # this is variable where the pwd of one user
+echo "pwd entered" $PASSWORD
+
 
